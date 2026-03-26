3295aa9b8641a48f5d8ae40a9588a5f830c919de
dymp1q <dmytrivmaryan2008@gmail.com>
2026-03-26 19:30:54 +0200

    implemented function for inserting node for correct place

---
+++ Linked Lists - Sorted Insert/code.py
@@ -0,0 +1,20 @@
+""" For your information:
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+"""
+
+def sorted_insert(head, data):
+    new_node = Node(data)
+    
+    if head is None or head.data >= data:
+        new_node.next = head
+        return new_node
+    
+    cur_node = head
+    while cur_node.next is not None and cur_node.next.data < data:
+        cur_node = cur_node.next 
+    new_node.next = cur_node.next
+    cur_node.next = new_node
+    return head
