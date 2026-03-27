aea00acd2703115c344d579956b3e90aefc51096
dymp1q <dmytrivmaryan2008@gmail.com>
2026-03-27 17:32:16 +0200

    Add alternating_split function for linked lists
    function for spliting lists

---
+++ Linked Lists - Alternating Split/code.py
@@ -0,0 +1,27 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    
+def alternating_split(head):
+    if head is None or head.next is None:
+        raise Exception("має бути хочаб 2 точки")
+        
+    first_head = head
+    second_head = head.next
+    curr1 = first_head
+    curr2 = second_head
+    
+    while curr1 and curr2:
+        curr1.next = curr2.next
+        curr1 = curr1.next
+        if curr1:
+            curr2.next = curr1.next
+            curr2 = curr2.next
+
+    return Context(first_head, second_head)
